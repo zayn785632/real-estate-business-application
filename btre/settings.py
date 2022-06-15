df1e6725220c3b5c51935d12994443169532c01c
Zayn <md.mostafa11@icloud.com>
2022-06-16 00:34:34 +0600

    new commit with database conn

--- btre/settings.py
+++ btre/settings.py
@@ -78,13 +78,16 @@ WSGI_APPLICATION = 'btre.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+DATABASES={
+   'default':{
+      'ENGINE':'django.db.backends.postgresql_psycopg2',
+      'NAME':'postgres',
+      'USER':'postgres',
+      'PASSWORD':'123456',
+      'HOST':'localhost',
+      'PORT':'5432',
+   }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.2/ref/settings/#auth-password-validators
